5d7000d7e0ac51c278048018d5618514508c7095
Tyler <claypooltyler00@gmail.com>
2020-10-12 04:37:11 -0500

    Added styling to forms

--- forms.py
+++ forms.py
@@ -1,14 +1,16 @@
 from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 
 class RegisterForm(forms.Form):
-    first_name = forms.CharField(label="First Name")
-    last_name = forms.CharField(label="Last Name")
-    email = forms.EmailField(label="Email Address")
-    password = forms.CharField(label="Password")
-    is_admin = forms.BooleanField(label="Are you an admin?")
-    phone_num = forms.IntegerField(label="Phone number")
-    salary = forms.FloatField(label="Salary")
+    first_name = forms.CharField(widget=TextInput(attrs={'placeholder' : 'First Name'}), label=False)
+    last_name = forms.CharField(widget=TextInput(attrs={'placeholder' : 'Last Name'}), label=False)
+    email = forms.EmailField(widget=TextInput(attrs={'placeholder' : ' Email Address'}), label=False)
+    password = forms.CharField(widget=PasswordInput(attrs={'placeholder' : 'Password'}), label=False)
+    comfirm_password = forms.CharField(widget=PasswordInput(attrs={"placeholder": "Comfirm Password"}), label=False)
+    is_admin = forms.BooleanField(label=False, required=False, widget=forms.HiddenInput)
+    phone_num = forms.IntegerField(widget=TextInput(attrs={'placeholder' : 'Phone Number'}), label=False)
+    salary = forms.FloatField(widget=TextInput(attrs={'placeholder' : 'Salary'}), label=False)
     
 class LoginForm(forms.Form):
-    email = forms.EmailField(label="Email Address",)
-    password = forms.CharField(label="Password")
+    email = forms.EmailField(widget=TextInput(attrs={'placeholder' : 'Email Address'}), label=False)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), label=False)
