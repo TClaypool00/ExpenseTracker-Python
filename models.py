3144b5e0b5dfd23f92b2f45cb027f63e96c09d9a
Tyler <claypooltyler00@gmail.com>
2020-10-11 23:40:19 -0500

    deleted managed_table line

--- models.py
+++ models.py
@@ -9,10 +9,9 @@ from django.db import models
 
 
 class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
+    name = models.CharField(unique=True, max_length=150, primary_key=True)
 
     class Meta:
-        managed = False
         db_table = 'auth_group'
 
 
@@ -21,7 +20,6 @@ class AuthGroupPermissions(models.Model):
     permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
 
     class Meta:
-        managed = False
         db_table = 'auth_group_permissions'
         unique_together = (('group', 'permission'),)
 
@@ -32,7 +30,6 @@ class AuthPermission(models.Model):
     codename = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
         db_table = 'auth_permission'
         unique_together = (('content_type', 'codename'),)
 
@@ -41,7 +38,7 @@ class AuthUser(models.Model):
     password = models.CharField(max_length=128)
     last_login = models.DateTimeField(blank=True, null=True)
     is_superuser = models.IntegerField()
-    username = models.CharField(unique=True, max_length=150)
+    username = models.CharField(unique=True, max_length=150, primary_key=True)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.CharField(max_length=254)
@@ -50,7 +47,6 @@ class AuthUser(models.Model):
     date_joined = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'auth_user'
 
 
@@ -59,7 +55,6 @@ class AuthUserGroups(models.Model):
     group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
 
     class Meta:
-        managed = False
         db_table = 'auth_user_groups'
         unique_together = (('user', 'group'),)
 
@@ -69,7 +64,6 @@ class AuthUserUserPermissions(models.Model):
     permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
 
     class Meta:
-        managed = False
         db_table = 'auth_user_user_permissions'
         unique_together = (('user', 'permission'),)
 
@@ -84,7 +78,6 @@ class Bills(models.Model):
     storeid = models.ForeignKey('Storeunion', models.DO_NOTHING, db_column='storeId')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'bills'
 
 
@@ -95,7 +88,6 @@ class Budget(models.Model):
     userid = models.ForeignKey('Users', models.DO_NOTHING, db_column='userId')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'budget'
 
 
@@ -109,7 +101,6 @@ class DjangoAdminLog(models.Model):
     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
 
     class Meta:
-        managed = False
         db_table = 'django_admin_log'
 
 
@@ -118,7 +109,6 @@ class DjangoContentType(models.Model):
     model = models.CharField(max_length=100)
 
     class Meta:
-        managed = False
         db_table = 'django_content_type'
         unique_together = (('app_label', 'model'),)
 
@@ -129,7 +119,6 @@ class DjangoMigrations(models.Model):
     applied = models.DateTimeField()
 
     class Meta:
-        managed = False
         db_table = 'django_migrations'
 
 
@@ -154,7 +143,6 @@ class Loan(models.Model):
     budgetid = models.ForeignKey(Budget, models.DO_NOTHING, db_column='budgetId')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'loan'
 
 
@@ -166,7 +154,6 @@ class Misc(models.Model):
     budgetid = models.ForeignKey(Budget, models.DO_NOTHING, db_column='budgetId')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'misc'
 
 
@@ -182,7 +169,6 @@ class Storeunion(models.Model):
     website = models.CharField(max_length=50)
 
     class Meta:
-        managed = False
         db_table = 'storeunion'
 
 
@@ -195,7 +181,6 @@ class Subscriptions(models.Model):
     budgetid = models.ForeignKey(Budget, models.DO_NOTHING, db_column='budgetId')  # Field name made lowercase.
 
     class Meta:
-        managed = False
         db_table = 'subscriptions'
 
 
@@ -205,10 +190,9 @@ class Users(models.Model):
     lastname = models.CharField(db_column='lastName', max_length=50)  # Field name made lowercase.
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=15)
-    isadmin = models.TextField(db_column='isAdmin')  # Field name made lowercase. This field type is a guess.
+    isadmin = models.BooleanField(db_column='isAdmin')  # Field name made lowercase. This field type is a guess.
     phonenum = models.IntegerField(db_column='phoneNum')  # Field name made lowercase.
     salary = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'users'
